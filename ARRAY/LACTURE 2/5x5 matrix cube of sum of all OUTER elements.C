f67d643758332288ce8b2e285a4996500f0a3617
JAYDEV2004 <144127232+JAYDEV2004@users.noreply.github.com>
2023-09-26 15:38:38 +0530

    Add files via upload

---
+++ ARRAY/LACTURE 2/5x5 matrix cube of sum of all OUTER elements.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int x[5][5]= {{05,10,15,20,25},{30,35,40,45,50},{55,60,65,70,75},{80,85,90,95,100},{105,110,115,120,125}};
+	
+	int i,j,sum=0;
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+				printf("%d",x[i][j]);
+		}
+		printf("\n");
+	}
+	printf("result the below");
+	
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			if(i==0 || i==4 || j==4 || j==0)
+			{
+				sum+=x[i][j];
+			}
+		}
+		printf("\n");
+	}
+	printf("THE SUM OF ALL THE OUTER ELEMENTS = %d",sum);
+	
+	return 0;
+}
